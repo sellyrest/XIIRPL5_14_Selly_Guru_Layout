498f0a5ff9707b3c9a4fc426490fa0cf08dc87bb
Selly Resty Wijayanti <sellyresty17@gmail.com>
2023-11-28 15:21:30 +0700

    menambah use state

--- screens/Login.js
+++ screens/Login.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
 import { Text, ScrollView, View, Image, StyleSheet, TextInput, Pressable, navigation, Button } from 'react-native';
 import CustomButton from '../components/CustomButton'
-import CustomTextInput from '../components/CustomInput';
+import CustomInput from '../components/CustomInput'
 
 const Login = ({navigation}) =>{
-    return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Image style={styles.logo} source={require('../assets/image/Frame.png')}/>
-      <CustomTextInput 
-      placeholder="Username"
-      placeholderColor="#000"/>
-      <CustomTextInput 
-      placeholder="Password"
-      placeholderColor="#000"/>
+    const [password, setPassword] = useState('');
+  
+    const handlePasswordChange = (text) => {
+      setPassword(text);
+    };
+return (
+  <ScrollView contentContainerStyle={styles.container}>
+    <Image style={styles.logo} source={require('../assets/image/Frame.png')}/>
+    <CustomInput 
+    placeholder="Username"
+    placeholderColor="#000"/>
+    <CustomInput 
+    placeholder="Password"
+    secureTextEntry={true}
+    value={password}
+    onChangeText={handlePasswordChange}
+    placeholderColor="#000"/>
       <CustomButton title='Login' bg='#F875AA' onPress={()=> navigation.navigate('Menu')}/>
       <View style={{ flexDirection: 'row', paddingTop:20 }}>
             <View style={{ height: 2, flex: 1, backgroundColor: 'lightgrey', alignSelf: 'center'}}></View>
-            <Text style={{ paddingHorizontal: 10, fontSize: 12, fontWeight: '', color:'grey' }}>Atau</Text>
+            <Text style={{ paddingHorizontal: 10, fontSize: 12, fontWeight: '500', color:'grey' }}>Atau</Text>
             <View style={{ height: 2, flex: 1, backgroundColor: 'lightgrey', alignSelf: 'center'}}></View>
         </View>
         <View style={{paddingTop:20}}>
